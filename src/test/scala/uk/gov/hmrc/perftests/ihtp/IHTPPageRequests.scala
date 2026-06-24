355a9be839d4bc6ee095c8f15ad4574eb44b657f
Divya-Dhall <189638788+Divya-Dhall@users.noreply.github.com>
2026-06-24 14:47:25 +0100

    comment added

--- src/test/scala/uk/gov/hmrc/perftests/ihtp/IHTPPageRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/ihtp/IHTPPageRequests.scala
@@ -162,13 +162,13 @@ object IHTPPageRequests extends BaseRequest {
         )
       )
 
-  def getLprIndividualNameChangePage: HttpRequestBuilder =    //Usingchangenameurltemporarlytoavoidaddresslookup
+  def getLprIndividualNameChangePage: HttpRequestBuilder =    //Usingchangenameurltoavoidaddresslookup
     http("Navigate to Enter the name of the Individual Page- Change page")
         .get(s"$baseUrl$route/change-name-lpr": String)
         .check(status.is(200))
         .check(saveCsrfToken())
 
-  def postLprIndividualNameChangePage(firstForename: String, surname: String): HttpRequestBuilder =  ////Usingchangenameurltemporarlytoavoidaddresslookup
+  def postLprIndividualNameChangePage(firstForename: String, surname: String): HttpRequestBuilder =  ////Usingchangenameurltoavoidaddresslookup
     http("Enter the full name of the person managing the estate Page-change")
       .post(s"$baseUrl$route/change-name-lpr": String)
       .formParam("csrfToken", csrfTokenExpr)
