c162926042044f7b48c8823c434b507d1c08e1e1
Divya-Dhall <189638788+Divya-Dhall@users.noreply.github.com>
2026-07-24 15:54:37 +0100

    IHTP-553

--- src/test/scala/uk/gov/hmrc/perftests/ihtp/IHTPPageRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/ihtp/IHTPPageRequests.scala
@@ -187,20 +187,20 @@ object IHTPPageRequests extends BaseRequest {
         header(locationHeaderExpr).is(s"$route/enter-name-pr-organisation": String)
       )
 
-  def getEnterNamePrOrganisationPage: HttpRequestBuilder =
+  def getChangeNamePrOrganisationPage: HttpRequestBuilder =
     http("Navigate to Enter the full name of the PR at <Organisation name> Page")
-      .get(s"$baseUrl$route/enter-name-pr-organisation": String)
+      .get(s"$baseUrl$route/change-name-pr-organisation": String)
       .check(status.is(200))
       .check(saveCsrfToken())
 
-  def postEnterNamePrOrganisationPage(firstForename: String, surname: String): HttpRequestBuilder =
+  def postChangeNamePrOrganisationPage(firstForename: String, surname: String): HttpRequestBuilder =
     http("Post Enter the full name of the PR at <Organisation name> Page")
-      .post(s"$baseUrl$route/enter-name-pr-organisation": String)
+      .post(s"$baseUrl$route/change-name-pr-organisation": String)
       .formParam("csrfToken", csrfTokenExpr)
       .formParam("firstForename", _ => firstForename)
       .formParam("surname", _ => surname)
       .check(status.is(303))
-      .check(header(locationHeaderExpr).is(s"$route/pr-submit-payment-notice": String))
+      .check(header(locationHeaderExpr).is(s"$route/check-your-answers": String))
 
   def getPrSubmitPaymentNoticePage: HttpRequestBuilder =
     http("Navigate to Did PR submit the payment notice? Page")
